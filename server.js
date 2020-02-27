e593b3d5627c8a16a56565fc8977f0401157d18d
mdulam <meghanareddy1506@gmail.com>
2020-02-26 20:33:16 -0500

    adding git ignore

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.use(cors());
 
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    
+    res.send('Hello World!')
+    //res.sendFile('index.html', {root : __dirname})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
