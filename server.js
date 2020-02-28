19439bbc3c02e2f7eaa7fd79d04629241b3b9ef7
mdulam <meghanareddy1506@gmail.com>
2020-02-27 23:31:20 -0500

    lambda function code modification

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 app.use(cors());
+const axios = require('axios');
+
 
 const port = 3000
 
 app.get('/', (req, res) => {
     
-    res.send('Hello World!')
-    //res.sendFile('index.html', {root : __dirname})
+     const info = axios.get('https://r3n249devl.execute-api.us-east-2.amazonaws.com/prod')
+    .then((response) => {
+        res.send(response.data);
+    });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
